5bc2c13abe238d1a0ecad9e37a73858ed810e765
prajitnarule <prajitnarule123@gmail.com>
2026-07-30 10:57:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
 
 export default defineConfig({
   plugins: [
@@ -11,6 +10,5 @@ export default defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
-    netlify(),
   ],
 })

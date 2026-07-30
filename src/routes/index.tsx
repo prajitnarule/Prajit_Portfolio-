b036f091dba12fcc0b9264dc7f3c9d5f74f0e22b
prajitnarule <prajitnarule123@gmail.com>
2026-07-30 11:29:49 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -138,7 +138,7 @@ function PortfolioHome() {
             </p>
           </div>
 
-          <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-5">
+          <div className="grid grid-cols-2 gap-4 lg:grid-cols-5">
             {portfolioVideos.map((video) => (
               <article
                 key={video.id}

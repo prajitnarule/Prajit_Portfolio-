816153f85a9aee3c1d2d006796dc19ad175b2cc5
prajitnarule <prajitnarule123@gmail.com>
2026-07-30 10:51:30 +0530

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,41 +1,15 @@
-import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
+import { createRouter } from '@tanstack/react-router'
 
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
 
-import '../styles.css'
+// Create a new router instance
+export const getRouter = () => {
+  const router = createRouter({
+    routeTree,
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+  })
 
-export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'prajit portfolio',
-      },
-      {
-        name: 'description',
-        content:
-          'Portfolio website for Prajit Narule, a short-form video editor specializing in reels, shorts, TikToks, captions, motion graphics, and sound design.',
-      },
-    ],
-  }),
-  shellComponent: RootDocument,
-})
-
-function RootDocument({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  )
+  return router
 }
